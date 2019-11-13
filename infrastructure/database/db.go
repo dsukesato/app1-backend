442e6c36fa907a6694e35a5da729b7c5ff025c90
daisuke sato <satodsuke@gmail.com>
2019-11-13 21:36:46 +0900

    db接続

--- infrastructure/database/db.go
+++ infrastructure/database/db.go
@@ -1,5 +1,46 @@
 package database
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	connectionName = os.Getenv("CLOUDSQL_CONNECTION_NAME")
+	user           = os.Getenv("CLOUDSQL_USER")
+	password       = os.Getenv("CLOUDSQL_PASSWORD")
+	database       = os.Getenv("CLOUDSQL_DATABASE")
+)
+
+const driverName = "mysql"
+
+func init() {
+	conn := fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/%s?parseTime=True", user, password, connectionName, database)
+
+	db, err := sql.Open(driverName, conn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if db != nil {
+			err := db.Close()
+			if err != nil {
+				panic(err)
+			} else {
+				fmt.Printf("DB connection success!!")
+			}
+		}
+	}()
+	_, err = db.Conn(context.Background())
+	if err != nil {
+		panic(err)
+	}
+}
+
 /*type postsRepository struct{}
 
 func (pr postsRepository) GetPosts() ([]*model.Post, error) {
